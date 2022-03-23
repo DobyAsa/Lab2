ef3c40a579e1512592831f09734b6dccc29ce0bc
DobyAsa <631118660@qq.com>
2022-03-23 18:19:37 +0800

    刚刚调试才发现，我的CharacterDivision因为不是传的引用，导致主函数里面的L其实内容已经被删完了，但是length还是40，导致因为delete了不是被分配的空间而被系统终止了。但是这个错误在Windows上不会报错，反而是在Mac上报的错。

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 #include "List.h"
 #include "List.cpp"
-#include "ListNode.h"
 #include "ListNode.cpp"
 
-void CharacterDivision(List<char> L);
+void CharacterDivision(List<char>& L);
 
 int main() {
     char input[] = "na4xjac5GsGAsya613F8dA5Hs4uD55hfaD4bjhk1";
@@ -17,7 +16,7 @@ int main() {
 }
 
 
-void CharacterDivision(List<char> L) {
+void CharacterDivision(List<char>& L) {
     List<char> L1, L2, L3;
     unsigned length = L.Length();
     for (int i = 0; i < length; i++) {
