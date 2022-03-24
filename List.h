d5c92a6410bd8bac004cd44cd04c4a8ff0946bd4
DobyAsa <631118660@qq.com>
2022-03-24 13:48:02 +0800

    吐了兄弟们，这个循环链表的插入跟删除我写了半天TAT

--- List.h
+++ List.h
@@ -3,7 +3,7 @@
 //
 #ifndef LAB2_LIST_H
 #define LAB2_LIST_H
-
+#include <iostream>
 #include "ListNode.h"
 
 template<typename T>
@@ -13,31 +13,207 @@ private:
     ListNode<T> tail;
     unsigned int length;
 public:
-    List();
+    List():tail(0, NULL), head(0, &tail), length(0) {}
 
-    List(T *Array, unsigned int size);
+    List(T *Array, unsigned int size):tail(0, NULL), head(0, &tail), length(0){
+        ListNode<T> *p = &head;
+        for (int i = 0; i < size; i++) {
+            p->SetNext(new ListNode<T>(Array[i], p->GetNext()));
+            p = p->GetNext();
+        }
+        length = size;
+    }
 
-    bool Insert(T ddata, unsigned int index);
+    bool Insert(T ddata, unsigned int index){
+        if (index > length) return false;
 
-    bool Delete(unsigned int index);
+        ListNode<T> *p = &head;
+        // 移动位置到要插入的地方
+        while (index > 0) {
+            p = p->GetNext();
+            index--;
+        }
+        p->SetNext(new ListNode<T>(ddata, p->GetNext()));
+        length++;
+        return true;
+    }
 
-    bool InsertAsFirst(T ddata);
+    bool Delete(unsigned int index){
+        if (index >= length) return false;
 
-    bool InsertAsLast(T ddata);
+        ListNode<T> *p = &head;
+        // 移动位置到要删除的地方
+        while (index > 0) {
+            p = p->GetNext();
+            index--;
+        }
+        ListNode<T> *q = p->GetNext();
+        p->SetNext(p->GetNext()->GetNext());
+        delete (q);
+        length--;
+        return true;
+    }
 
-    bool IsEmpty();
+    bool InsertAsFirst(T ddata){return Insert(ddata, 0);}
 
-    unsigned int Length();
+    bool InsertAsLast(T ddata){return Insert(ddata, length - 1);}
 
-    void Show();
+    bool IsEmpty(){
+        return length == 0;
+    }
 
-    T GetData(unsigned int index);
+    unsigned int Length(){
+        return length;
+    }
 
-    ListNode<T> *GetFirst();
+    void Show(){
+        ListNode<T> *p = head.GetNext();
+        for (int i = 0; i < length; i++) {
+            std::cout<<p->GetData()<<"| ";
+            p = p->GetNext();
+        }
+        printf("size: %d \n", length);
+    }
 
-    ListNode<T> *Find(T target);
+    T GetData(unsigned int index){
+        if (index >= length) {
+            printf("Index out of range!");
+            return -1;
+        }
 
-    ~List();
+        ListNode<T> *p = head.GetNext();
+        while (index > 0) {
+            p = p->GetNext();
+            index--;
+        }
+        return p->GetData();
+    }
+
+    ListNode<T> *GetFirst(){
+        return head.GetNext();
+    }
+
+    ListNode<T> *Find(T target){
+        ListNode<T> *p = head.GetNext();
+        while (p != &tail) {
+            if (p->GetData() == target) return p;
+            else p = p->GetNext();
+        }
+        return NULL;
+    }
+
+    ~List(){
+        while(length){
+            Delete(0);
+        }
+    }
 };
 
+
+template <typename T>
+class CircleList {
+private:
+    ListNode<T>* head;
+    ListNode<T>* last;
+    unsigned int size;
+public:
+    CircleList():head(NULL), size(0), last(NULL){}
+
+    CircleList(T* Array, unsigned int ssize):head(NULL), size(0), last(NULL){
+        if (ssize > 0){
+            head = new ListNode<T>(Array[0], NULL);
+            ListNode<T> *p = head;
+            for (int i = 1; i < ssize; i++) {
+                p->SetNext(new ListNode<T>(Array[i], p->GetNext()));
+                p = p->GetNext();
+                last = p;
+                size++;
+            }
+            p->SetNext(head);
+        }
+    }
+
+    void Insert(T ddata, unsigned int index){
+        if (size == 0) {
+            head = new ListNode<T>(ddata, NULL);
+            head->SetNext(head);
+            last = head;
+        }else{
+            if (index == 0){
+                last->SetNext(new ListNode<T>(ddata, head));
+                head = last->GetNext();
+            }else {
+                ListNode<T>* p = head;
+                ListNode<T>* q = last;
+                while (index > 0) {
+
+                    q = p;
+                    p = p->GetNext();
+                    index--;
+                }
+                q->SetNext(new ListNode<T>(ddata, p));
+            }
+        }
+        size++;
+    }
+
+    void Delete(unsigned int index){
+        ListNode<T>* p = head;
+        ListNode<T>* q = last;
+        while(index > 0){
+            q = p;
+            p = p->GetNext();
+            index--;
+        }
+        q->SetNext(p->GetNext());
+        delete(q);
+        size--;
+    }
+
+    void Show(){
+        ListNode<T>* p = head;
+        for (int i = 0; i < size; i++){
+            std::cout<<p->GetData()<<"| ";
+            p = p->GetNext();
+        }
+    }
+
+    ListNode<T>* FindNode(T ddata){
+        ListNode<T>* p = head;
+        while(p->GetNext() != head){
+            if (p->GetData() == ddata){
+                return p;
+            }else{
+                p = p->GetNext();
+            }
+        }
+        if (p->GetData() == ddata)  return p;
+        return NULL;
+    }
+
+    void DeleteNode(ListNode<T>* node){
+        ListNode<T>* p = head;
+        ListNode<T>* q = last;
+        while(p != node){
+            q = p;
+            p = p->GetNext();
+        }
+        q->SetNext(p->GetNext());
+        if (p == last)  last = q;
+        if (p == head)  head = q->GetNext();
+        delete(p);
+        size--;
+    }
+
+    unsigned int Size(){
+        return size;
+    }
+
+    ~CircleList(){
+        while(size){
+            Delete(0);
+        }
+    }
+
+};
 #endif //LAB2_LIST_H
--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
 
 #include <iostream>
 #include "List.h"
-#include "List.cpp"
 #include "ListNode.cpp"
-
-void CharacterDivision(List<char>& L);
+void CharacterDivision(char* input, unsigned int size);
+void JosephusCircle(int n, int k, int m);
 
 int main() {
     char input[] = "na4xjac5GsGAsya613F8dA5Hs4uD55hfaD4bjhk1";
-    List<char> L(input, 40);
-    L.Show();
-    CharacterDivision(L);
+    CharacterDivision(input, 40);
+
+//    JosephusCircle(41, 1, 3);
 
     return 0;
 }
 
 
-void CharacterDivision(List<char>& L) {
+
+
+void CharacterDivision(char* input, unsigned int size) {
+    List<char> L(input, size);
+    printf(" L: ");
+    L.Show();
     List<char> L1, L2, L3;
     unsigned length = L.Length();
     for (int i = 0; i < length; i++) {
@@ -30,10 +34,29 @@ void CharacterDivision(List<char>& L) {
         }
         L.Delete(0);
     }
-    printf("L1:");
+    printf("L1: ");
     L1.Show();
-    printf("L2:");
+    printf("L2: ");
     L2.Show();
-    printf("L3:");
+    printf("L3: ");
     L3.Show();
+};
+
+void JosephusCircle(int n, int k, int m){
+    CircleList<int> L;
+    for (int i = n; i > 0; i--){
+        L.Insert(i, 0);
+    }
+    ListNode<int>* begin = L.FindNode(k);
+    while(L.Size()){
+        int count = m-1;
+        while(count){
+            begin = begin->GetNext();
+            count--;
+        }
+        std::cout<<begin->GetData()<<std::endl;
+        ListNode<int>* a = begin;
+        begin = begin->GetNext();
+        L.DeleteNode(a);
+    }
 };
